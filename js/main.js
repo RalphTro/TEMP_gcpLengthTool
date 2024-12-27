ded326581eaab208487c8b8d372b20b15a687c20
RalphTro <46958354+RalphTro@users.noreply.github.com>
2024-12-27 14:20:54 +0100

    First draft

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	// when document loaded:
+
+	$("#input").on("mousedown",function() {
+
+	});
+
+	$("#div5").on("click",function() {
+
+	});
+
+	$("#gtin").on("click",function() {
+
+	});
+
+	$("#sscc").on("click",function() {
+
+	});
+
+	$("#gln").on("click",function() {
+
+	});
+
+	$("#div31").on("click",function() {
+
+	});
+
+});
